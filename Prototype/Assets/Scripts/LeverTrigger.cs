c01e821274ab6ccad45daee7398911db80c2f699
Kim JinWook <dpscbvls8358@naver.com>
2025-06-27 14:48:14 +0900

    GitHubTest_1

--- Prototype/Assets/Scripts/LeverTrigger.cs
+++ Prototype/Assets/Scripts/LeverTrigger.cs
@@ -8,6 +8,7 @@ public class LeverTrigger : MonoBehaviour
     public GameObject[] targetObject;
     private float duration = 10f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // githubTest
     void Start()
     {
         
