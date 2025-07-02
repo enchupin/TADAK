f9eb28136cacb6d98c3278eab230f27696065a4f
enchupin <dpscbvls8358@naver.com>
2025-07-02 17:46:49 +0900

    WindZone_1

--- Prototype/Assets/Scripts/WindZone.cs
+++ Prototype/Assets/Scripts/WindZone.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class WindZone : MonoBehaviour
 {
+    public float windForce = 250f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +13,15 @@ public class WindZone : MonoBehaviour
     void Update()
     {
         
+    }
+
+
+    private void OnTriggerStay2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            Rigidbody2D rb = other.attachedRigidbody;
+            if (rb != null) {
+                rb.AddForce(Vector2.left * windForce);
+            }
+        }
     }
 }
