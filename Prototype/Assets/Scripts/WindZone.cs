f78522207db47e63dc2bb88d14e2cc9591f7b327
enchupin <dpscbvls8358@naver.com>
2025-07-04 23:23:06 +0900

    WindZone_3

--- Prototype/Assets/Scripts/WindZone.cs
+++ Prototype/Assets/Scripts/WindZone.cs
@@ -2,26 +2,30 @@ using UnityEngine;
 
 public class WindZone : MonoBehaviour
 {
-    public float windForce = 250f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private BoxCollider2D box;
+    private Vector2 rightEdge;
+    private Vector2 leftEdge;
+    private float boxLength;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private float maxWindForce = 400f;
 
+    private void Awake() {
+        box = GetComponent<BoxCollider2D>();
+        Vector2 center = (Vector2)transform.position + box.offset;
+        rightEdge = center + Vector2.right * box.size.x * 0.5f * transform.localScale.x;
+        leftEdge = center - Vector2.right * box.size.x * 0.5f * transform.localScale.x;
+        boxLength = (rightEdge - leftEdge).magnitude;
+    }
 
     private void OnTriggerStay2D(Collider2D other) {
-        if (other.CompareTag("Player")) {
-            Rigidbody2D rb = other.attachedRigidbody;
-            if (rb != null) {
-                rb.AddForce(Vector2.left * windForce);
-            }
-        }
+        if (!other.CompareTag("Player")) return;
+
+        Rigidbody2D rb = other.attachedRigidbody;
+        if (rb == null) return;
+
+        Vector2 playerPos = other.transform.position;
+        float posRatio = Mathf.Clamp01((playerPos.x - leftEdge.x) / boxLength);
+        float windForce = Mathf.Lerp(0f, maxWindForce, posRatio);
+        rb.AddForce(Vector2.left * windForce, ForceMode2D.Force);
     }
 }
