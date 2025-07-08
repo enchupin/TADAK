df6a408ec33e0fd0cda39ea308f453328ef8e588
enchupin <dpscbvls8358@naver.com>
2025-07-08 17:31:29 +0900

    WindZone_4

--- Prototype/Assets/Scripts/WindZone.cs
+++ Prototype/Assets/Scripts/WindZone.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class WindZone : MonoBehaviour
 {
@@ -6,8 +7,9 @@ public class WindZone : MonoBehaviour
     private Vector2 rightEdge;
     private Vector2 leftEdge;
     private float boxLength;
+    private bool windCooldown = false;
 
-    private float maxWindForce = 400f;
+    public float maxWindForce = 4000f;
 
     private void Awake() {
         box = GetComponent<BoxCollider2D>();
@@ -16,16 +18,30 @@ public class WindZone : MonoBehaviour
         leftEdge = center - Vector2.right * box.size.x * 0.5f * transform.localScale.x;
         boxLength = (rightEdge - leftEdge).magnitude;
     }
-
     private void OnTriggerStay2D(Collider2D other) {
+        if (windCooldown) return;
         if (!other.CompareTag("Player")) return;
 
         Rigidbody2D rb = other.attachedRigidbody;
         if (rb == null) return;
 
-        Vector2 playerPos = other.transform.position;
-        float posRatio = Mathf.Clamp01((playerPos.x - leftEdge.x) / boxLength);
-        float windForce = Mathf.Lerp(0f, maxWindForce, posRatio);
-        rb.AddForce(Vector2.left * windForce, ForceMode2D.Force);
+        PlayerControl pc = other.GetComponent<PlayerControl>();
+        if (pc == null) return;
+
+        StartCoroutine(WindCooldown(rb, pc));
     }
+
+    IEnumerator WindCooldown(Rigidbody2D rb, PlayerControl pc) {
+        windCooldown = true;
+        pc.canMoveState = false;
+
+        rb.AddForce(Vector2.left * maxWindForce, ForceMode2D.Impulse);
+
+        yield return new WaitForSeconds(5f);
+
+        pc.canMoveState = true;
+        windCooldown = false;
+    }
+
+
 }
