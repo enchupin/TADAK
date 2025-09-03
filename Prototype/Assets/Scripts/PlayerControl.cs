8ae0b6d6a0f4948833cd24bdbc26592d6e6e4f0b
Duhyun Kim <wooojin0316@naver.com>
2025-09-03 16:55:49 +0900

    slide

--- Prototype/Assets/Scripts/PlayerControl.cs
+++ Prototype/Assets/Scripts/PlayerControl.cs
@@ -3,16 +3,20 @@ using System.Net;
 using Unity.VisualScripting;
 using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerControl : MonoBehaviour {
     private Rigidbody2D rb;
-    private bool isGrounded = true;
+    private bool isGrounded = false;
 
     private float speed; // 최종 이동속도
     private float maxWalkSpeed = 5.0f; // 이동속도 계산 (속도 바꾸려면 이거 초기화 값 수정)
     private float jumpForce = 6.0f; // 점프력
     private float horizontalKey;
     public int gravityMode = 0; // 중력의 작용 방향 0 = 아래, 1 = 위, 2 = 좌측, 3 = 우측
+    public float maxSlideSpeed = 100f;
+    public float maxSlopeAngle = 30f;
+    public float groundFriction = 15f; // 지면 마찰력
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
@@ -30,17 +34,10 @@ public class PlayerControl : MonoBehaviour {
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        //if (collision.gameObject.CompareTag("Ground"))
-        //{
-        //    foreach (ContactPoint2D contact in collision.contacts)
-        //    {
-        //        slopeNormal = contact.normal;
-        //        break;
-        //    }
-        //}
         foreach (ContactPoint2D contact in collision.contacts)
         {
             slopeNormal = contact.normal;
+            // Debug.Log("바닥의 수직 벡터 : " + slopeNormal.x + " , " + slopeNormal.y);
             break;
         }
     }
@@ -57,24 +54,57 @@ public class PlayerControl : MonoBehaviour {
             Vector3 scale = transform.localScale;
             scale.x = horizontalKey > 0 ? 1 : -1;
             transform.localScale = scale;
+
+            this.rb.linearVelocity = new Vector2(speed, this.rb.linearVelocity.y);
         }
         else
         {
-            if (slopeNormal != Vector2.up)
+            if (isGrounded)
             {
-                Vector2 slopeTangent = new Vector2(slopeNormal.y, -slopeNormal.x).normalized;
+                float slopeAngle = Vector2.Angle(slopeNormal, Vector2.up);
 
-                float slideAmount = Vector2.Dot(Physics2D.gravity.normalized, slopeTangent);
+                if (slopeNormal != Vector2.up)
+                {
+                    Vector2 slideDirection = new Vector2(slopeNormal.y, -slopeNormal.x).normalized;
 
-                speed = slideAmount * 5f;  // 슬라이드 속도
+                    float slideAmount = Vector2.Dot(Physics2D.gravity.normalized, slideDirection);
+
+                    speed = slideAmount * 5f;  // 슬라이드 속도
+
+                    float finalSlideSpeed = speed;
+
+                    rb.linearVelocity = slideDirection * finalSlideSpeed;
+                }
+                else if (slopeNormal == Vector2.up)
+                {
+                    Vector2 frictionVelocity = rb.linearVelocity;
+                    frictionVelocity.x = Mathf.MoveTowards(rb.linearVelocity.x, 0f, groundFriction * Time.deltaTime);
+
+                    rb.linearVelocity = frictionVelocity;
+                }
+                //if (slopeAngle > maxSlopeAngle) // 미끄러져야 할 경사면이라면
+                //{
+                //    // 미끄러짐 로직 (이전과 동일)
+                //    Vector2 slideDirection = new Vector2(slopeNormal.y, -slopeNormal.x).normalized;
+                //    float slideAmount = Vector2.Dot(Physics2D.gravity.normalized, slideDirection);
+                //    float finalSlideSpeed = maxSlideSpeed * slideAmount;
+                //    rb.linearVelocity = slideDirection * finalSlideSpeed;
+                //}
+                //else // 서 있을 수 있는 평지 또는 완만한 경사면이라면
+                //{
+                //    // 멈춤 로직 (이전과 동일)
+                //    Vector2 frictionVelocity = rb.linearVelocity;
+                //    frictionVelocity.x = Mathf.MoveTowards(rb.linearVelocity.x, 0f, groundFriction * Time.deltaTime);
+                //    rb.linearVelocity = frictionVelocity;
+                //}
             }
-            else if (slopeNormal == Vector2.up)
+            else
             {
+                this.rb.linearVelocity = new Vector2(0, this.rb.linearVelocity.y);
                 speed = 0;
             }
+            
         }
-        
-        this.rb.linearVelocity = new Vector2(speed, this.rb.linearVelocity.y);
     }
 
     void PlayerJump() {
@@ -119,6 +149,4 @@ public class PlayerControl : MonoBehaviour {
             isGrounded = true;
         }
     }
-
-    
 }
